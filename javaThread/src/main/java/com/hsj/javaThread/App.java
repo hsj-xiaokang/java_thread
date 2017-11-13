4b34dd4c6d2aa5cc7584624253e2caf86f2f2970
2356899074@qq.com <2356899074@qq.com>
2017-11-13 15:08:54 +0800

    源码及相关的博客

---
+++ javaThread/src/main/java/com/hsj/javaThread/App.java
@@ -0,0 +1,19 @@
+package com.hsj.javaThread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * entry
+ * 博客：https://www.cnblogs.com/wxd0108/p/5479442.html
+ * @Description:TODO
+ * @author:hsj qq:2356899074
+ * @time:2017年11月13日 下午2:15:18
+ */
+@SpringBootApplication
+public class App{
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
